c48d911ec702ae990521f537859b8a1c9815c81f
Zag <zaagagon@gmail.com>
2020-01-11 17:47:48 -0500

    primeros cambios en zag p1 cpp y c era necesario el comando git init

---
+++ P1.c
@@ -0,0 +1,2 @@
+// repasando programcion c
+
---
+++ P1.cpp
@@ -0,0 +1,15 @@
+// repasando programcion c
+
+// al iniciar la progrmacion visual descarga las extensiones necesarias 
+
+//descarga 3 extensiones par la programacion en c /c++
+//otra prueba
+
+
+#include<stdio.h> 
+
+
+void main {
+
+    printf("hola zag en el 2020");
+}
